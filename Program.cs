02a32c67c9111275b43bcf2a542e2d428d32ce9a
Pablo Sousa <pablosousa172@gmail.com>
2023-12-21 13:31:46 -0300

    trilha-net-poo-desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("1199887755", "3310", "135458P158", 6000);
+Smartphone iPhone = new Iphone("15324518541", "15 Pro Max", "1945722P1848", 128000);
+
+Console.WriteLine("=============== Testing Nokia ====================");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+Console.WriteLine("=================================================");
+
+Console.WriteLine("");
+
+Console.WriteLine("=============== Testing IPhone ==================");
+iPhone.Ligar();
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
+Console.WriteLine("=================================================");
